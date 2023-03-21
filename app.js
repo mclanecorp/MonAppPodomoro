e47a576519f8cfc1b8603223f422d4bc8bd43ea4
mclanecorp <darkmienworld@gmail.com>
2023-03-21 10:40:18 +0100

    avancer 25%

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const minute = document.getElementById("minute");
 const seconde = document.getElementById("seconde");
 const reset = document.getElementById("reset");
 const stop = document.getElementById("stop");
-let var_seconde = 00;
-let var_minute = 3;
+const color_time = document.getElementById("color_time");
+const select = document.getElementById("select");
+
+let time_color = 100;
+let calcul_time = 100 / (select.value * 60);
+let var_seconde = 59;
+let var_minute = select.value - 1;
 let time_end = false;
 let time_interval = 1000;
 
@@ -14,36 +19,46 @@ setInterval(secondes, time_interval);
 
 reset.addEventListener("click", () => {
   var_seconde = 59;
-  var_minute = 2;
+  var_minute = select.value - 1;
+  console.log(select);
   minute.innerText = var_minute;
   seconde.innerText = var_seconde;
   time_end = false;
+  time_color = 100;
+  color_time.style.width = time_color + "%";
 });
 stop.addEventListener("click", () => {
   var_seconde = 00;
   var_minute = 0;
   minute.innerText = var_minute;
   seconde.innerText = var_seconde + "0";
-  time_interval = 0;
+  time_color = 0;
+  color_time.style.width = time_color + "%";
 });
 
 function secondes() {
   if (time_end != true) {
     if (var_seconde == 0) {
-      minutes();
       var_seconde = 59;
+      minutes();
     } else {
       if (var_seconde <= 10) {
         var_seconde -= 1;
         seconde.innerText = "0" + var_seconde;
+        time_color -= calcul_time;
+        color_time.style.width = time_color + "%";
       } else {
         var_seconde -= 1;
         seconde.innerText = var_seconde;
+        time_color -= calcul_time;
+        color_time.style.width = time_color + "%";
       }
     }
   } else {
     var_seconde = 0;
     var_minute = 0;
+    time_color = 0;
+    color_time.style.width = time_color + "%";
   }
 }
 
